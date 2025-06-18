063f04913f1f2669f68d3cbeadef75ce11bec133
Likhilesh <likbalpande@example.com>
2025-06-18 11:25:34 +0530

    feat: add initial HTML structure for BOM & DOM lesson

---
+++ day3/BOM_and_DOM/app.js
@@ -0,0 +1,10 @@
+// console.log("window", window);
+
+// console.log(window.document);
+// console.dir(window.document.children[0].children[1].children[0].innerHTML);
+
+// ----------------------------------------------------
+// 1. document.getElement_s_ByTagName --> [ ] (iterable) (HTML Collection)
+
+const headings = document.getElementsByTagName("h2");
+console.log("headings:", headings);
