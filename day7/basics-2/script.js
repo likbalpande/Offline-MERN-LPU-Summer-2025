2c9c0d4f4fd5d39a4ec4c37eff4476805b7d4418
Likhilesh <likbalpande@example.com>
2025-06-24 13:45:20 +0530

    REACT PROPs & Components

--- day7/basics-2/script.js
+++ day7/basics-2/script.js
@@ -34,23 +34,63 @@
 
 // ----------------------------------------------------
 
+// const domRoot = document.getElementById("root");
+// const reactRoot = ReactDOM.createRoot(domRoot);
+
+// const Card1 = (title) => {
+//     return (
+//         <div className="card">
+//             <h1>{title}</h1>
+//             <p>Description</p>
+//         </div>
+//     );
+// };
+
+// const Card2 = (
+//     <div className="card">
+//         <h1>------Done-----</h1>
+//         <p>Description</p>
+//     </div>
+// );
+
+// const Container = (
+//     <div>
+//         {Card1("Hello")}
+//         {Card1("Notice...")}
+//         {Card1("Warning!")}
+//         {Card2}
+//     </div>
+// );
+
+// reactRoot.render(Container);
+
+// --------------------------------------------
+
 const domRoot = document.getElementById("root");
 const reactRoot = ReactDOM.createRoot(domRoot);
 
-const Card1 = () => {
+const Card1 = (obj) => {
     return (
         <div className="card">
-            <h1>Invitation</h1>
+            <h1>{obj.title}</h1>
             <p>Description</p>
         </div>
     );
 };
 
+const Card2 = (
+    <div className="card">
+        <h1>------Done-----</h1>
+        <p>Description</p>
+    </div>
+);
+
 const Container = (
     <div>
-        {Card1}
+        {Card1({ title: "Hello" })}
+        <Card1 title="Notice"></Card1>
+        {Card1({ title: "Warning!" })}
         {Card2}
-        {Card3}
     </div>
 );
 
