1c26471664851473737a1592b2cc4e0e6c7120a3
Likhilesh <likbalpande@example.com>
2025-06-25 14:58:51 +0530

    useState Hook

--- day8/react-state-app/src/pages/HomePage.jsx
+++ day8/react-state-app/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import { Navbar } from "../components/Navbar";
+import { SearchBox } from "../components/SearchBox";
 
 const HomePage = () => {
     return (
         <div>
             <Navbar />
             <h1>HomePage!</h1>
+            <SearchBox />
         </div>
     );
 };
