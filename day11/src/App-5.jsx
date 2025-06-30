f993b2eaa5645ad494f909243c16f6ffc37c38d1
Likhilesh <likbalpande@example.com>
2025-06-30 14:03:57 +0530

    PLAY / PAUSE feature with more clarity and less code

--- day11/src/App-5.jsx
+++ day11/src/App-5.jsx
@@ -1,27 +1,19 @@
 import { useEffect, useState } from "react";
 
 const App = () => {
-    const [timeInSec, setTimeInSec] = useState(0);
+    const [timeInSec, setTimeInSec] = useState(0); // if you want value to be stored + rendered on the screen properly
     const [isTimerRunning, setIsTimerRunning] = useState(true);
 
     useEffect(() => {
+        console.log("--- empty ---");
+    }, []); // empty dependency array
+    //         ---> this effect will run only on first-render / initial render
+
+    useEffect(() => {
         console.log("Timer running state changed", isTimerRunning);
-
-        let intervalId = null;
-        if (isTimerRunning) {
-            intervalId = setInterval(() => {
-                // setTimeInSec((prev) => prev + 1);
-                setTimeInSec((prev) => {
-                    return prev + 1;
-                });
-            }, 1000); //st1
-        }
-
-        return () => {
-            console.log("Cleaning up interval...");
-            clearInterval(intervalId);
-        };
-    }, [isTimerRunning]);
+    }, [isTimerRunning]); // dependency array
+    //                       ---> this effect will run only on first-render / initial render
+    //                       ---> whenever the value of any variable inside dependency changes
 
     const handlePause = () => {
         if (isTimerRunning) {
