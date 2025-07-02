a456b79e19e2c03d2cc7e580f0656e6d09868648
Likhilesh <likbalpande@example.com>
2025-07-02 14:50:41 +0530

    fix: handle empty strings and improve text formatting functions

--- day13-nodeJS/understanding-modules/text-formatter.js
+++ day13-nodeJS/understanding-modules/text-formatter.js
@@ -4,24 +4,24 @@ console.log("------ ! Loading text-formatter.js file ---------");
 const username = "Mohan";
 
 const getCapitalizedText = (txt) => {
-    const words = txt.split(" ");
+    const words = txt.trim().split(" ");
     const resArr = words.map((elem) => {
-        const firstChar = elem[0].toUpperCase(); //g
-        const remainingWord = elem.slice(1).toLowerCase(); //OOD
+        const firstChar = elem[0]?.toUpperCase(); //g
+        const remainingWord = elem?.slice(1)?.toLowerCase(); //OOD
         return `${firstChar}${remainingWord}`; // `Good`
     });
     return resArr.join(" ");
 };
 
 const getCamelCaseText = (txt) => {
-    const words = txt.split(" ");
+    const words = txt.trim().split(" ");
     const resArr = words.map((elem, idx) => {
         if (idx !== 0) {
-            const firstChar = elem[0].toUpperCase(); //g
-            const remainingWord = elem.slice(1).toLowerCase(); //OOD
+            const firstChar = elem[0]?.toUpperCase(); //g
+            const remainingWord = elem.slice(1)?.toLowerCase(); //OOD
             return `${firstChar}${remainingWord}`; // `Good`
         } else {
-            return elem.toLowerCase();
+            return elem?.toLowerCase();
         }
     });
     return resArr.join("");
