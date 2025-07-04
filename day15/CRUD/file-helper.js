32216abbb73caef75ed5a9ce0cf83540f8c896ae
Likhilesh <likbalpande@example.com>
2025-07-04 10:36:07 +0530

    CRUD operations done: Delete is H.W.

--- day15/CRUD/file-helper.js
+++ day15/CRUD/file-helper.js
@@ -20,11 +20,6 @@ const saveObjectToArrayInFile = async (obj, filePath) => {
 };
 
 const getAllDataFromArrayFromFile = async (filePath) => {
-    // read the file
-    // try to convert it into JS object using JSON.parse()
-    // if there is any error --> file is empty
-    //                       --> return empty array
-    // if it is able to parse --> return the parsed array
     try {
         const txt = await fsPromises.readFile(filePath, "utf-8");
         try {
@@ -47,15 +42,31 @@ const getAllDataFromArrayFromFile = async (filePath) => {
     }
 };
 
-const editObjectFromArrayFromFile = async (newObj, idx, filePath) => {
-    // read the file
-    // try to convert it into JS object using JSON.parse()
-    // if there is any error --> file is empty
-    //                       --> show the error in the console ---> object does not exists
-    // if it is able to parse --> get the particular from array using index
-    //                        --> change the object as you want
-    //                        --> ::save to the file::
+const editObjectFromArrayFromFile = async (newObjProperties, elemId, filePath) => {
+    const arr = await getAllDataFromArrayFromFile(filePath);
+    // if it is able to parse --> get the particular Object from array using id
+    const idx = arr.findIndex((elem) => {
+        if (elem.id === elemId) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+
+    if (idx === -1) {
+        console.error("🔴 Invalid Id. No object found with given Id!");
+        return arr;
+    }
+
+    // change the object as you want
+    const currentObject = arr[idx];
+    const newObject = { ...currentObject, ...newObjProperties };
+    arr[idx] = newObject;
+
+    // save to the file
+    saveArrayToFile(arr, filePath);
 };
+
 const deleteObjectFromArrayFromFile = async (idx, filePath) => {
     // read the file
     // try to convert it into JS object using JSON.parse()
--- day15/CRUD/main.js
+++ day15/CRUD/main.js
@@ -8,17 +8,17 @@ const {
 const main = async () => {
     const filePath = "./my-data.json";
 
-    await saveObjectToArrayInFile({ name: "Likhilesh", city: "Delhi", score: 65 }, filePath);
-    const resp = await getAllDataFromArrayFromFile(filePath);
-    console.log("all data --> ", resp);
+    // await saveObjectToArrayInFile({ name: "Likhilesh", city: "Delhi", score: 65 }, filePath);
+    // const resp = await getAllDataFromArrayFromFile(filePath);
+    // console.log("all data --> ", resp);
 
-    // saveObjectToArrayInFile({ name: "Anuj", city: "Noida" }, filePath);
-    // const resp2 = await getAllDataFromArrayFromFile(filePath);
-    // console.log("all data --> ", resp2);
+    saveObjectToArrayInFile({ name: "Anuj", city: "Noida", score: 50 }, filePath);
+    const resp2 = await getAllDataFromArrayFromFile(filePath);
+    console.log("all data --> ", resp2);
 
-    // editObjectFromArrayFromFile({ city: "Pune" }, 0, filePath);
-    // const resp3 = await getAllDataFromArrayFromFile(filePath);
-    // console.log("all data --> ", resp3);
+    editObjectFromArrayFromFile({ city: "Pune" }, resp2[0].id, filePath);
+    const resp3 = await getAllDataFromArrayFromFile(filePath);
+    console.log("all data --> ", resp3);
 
     // saveObjectToArrayInFile({ name: "Mohan", city: "Jaipur" }, filePath);
     // const resp4 = await getAllDataFromArrayFromFile(filePath);
